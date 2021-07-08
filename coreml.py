c67ca4265baa7e46d2e8b98f6740a1d7fa4bb26a
Vladimir Koltunov <progrand@gmail.com>
2021-07-08 16:38:41 +0300

    added convertv4.py

--- coreml.py
+++ coreml.py
@@ -2,6 +2,13 @@
 
 import sys
 import coremltools
+from coremltools.proto import NeuralNetwork_pb2
+
+def convert_mish(layer):
+	params = NeuralNetwork_pb2.CustomLayerParams()
+	params.className = "Mish"
+	params.description = "Mish Activation Layer"
+	return params
 
 
 if len(sys.argv) == 1:
@@ -12,12 +19,14 @@ coreml_model = coremltools.converters.keras.convert(
 	sys.argv[1] + '.h5',
 	input_names='image',
 	image_input_names='image',
-	image_scale=1/255.
+	image_scale=1/255.,
+	add_custom_layers=True,
+	custom_conversion_functions={ "Mish": convert_mish }
 )
 
 coreml_model.author = 'mtbo.org'
 coreml_model.license = 'mtbo.org'
-coreml_model.short_description = "The Tiny YOLOv3 network"
+coreml_model.short_description = "The YOLOv4 network"
 
 print(coreml_model)
 
