f730d8c403b00d76d3624fc6e17e07d0a3c35eb6
vladimir_koltunov <progrand@gmail.com>
2022-03-23 15:41:16 +0300

    fix test

--- scratch/make_mlpackage.sh
+++ scratch/make_mlpackage.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#sh ../prepare_cfg.sh scratch.cfg scratch_t.cfg
-sh ../prepare_cfg.sh scratch_m7.cfg scratch_m7_t.cfg
+sh ../prepare_cfg.sh scratch.cfg scratch_t.cfg
 
-python ../convert_v4.py -n names.txt -c scratch_m7_t.cfg -w scratch_m7.weights -m scratch.mlpackage -l BGR	
+python ../convert_v4.py -n names.txt -c scratch_t.cfg -w scratch.weights -m scratch.mlpackage -l BGR	
